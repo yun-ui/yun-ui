80dd26a9bca507311a79e184d87f87151e2b1813
haoyang_hu <hhyuestc@gmail.com>
2017-01-04 14:08:00 +0800

    add return event in index page

---
+++ packages/form/index.js
@@ -0,0 +1,2 @@
+import form from './src/form.vue'
+module.exports = form
---
+++ packages/form/src/form.vue
@@ -0,0 +1,20 @@
+<template>
+    <div :class="UIName+'-form'">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    import {UIName} from 'mixins'
+    export default {
+        name: 'yun-form',
+        mixins: [UIName],
+        props: {},
+        methods: {}
+    }
+
+</script>
+
+<style lang="less" rel="stylesheet/less">
+    @import "../../style/stylesheets/cell";
+</style>
