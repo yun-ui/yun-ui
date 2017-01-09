384cc44bfc7c52d8fb375307c3f77ed1dbf46031
haoyang_hu <hhyuestc@gmail.com>
2017-01-09 16:30:47 +0800

    Feature demo:reject url while not matching route config

--- demo/entry.js
+++ demo/entry.js
@@ -14,8 +14,31 @@ Vue.use(VueRouter)
 const router = new VueRouter({
     routes: routes.route
 })
+function isConfigedRoute (path) {
+    let result = false
+    routes.route.map(route => {
+        // 对子路由的判断
+        if (route.children) {
+            route.children.map(child => {
+                if (route.path + '/' + child.path == path) {
+                    result = true
+                }
+            })
+        }
+        if (route.path == path) {
+            result = true
+        }
+    })
+    return result
+}
+router.beforeEach((to, from, next) => {
+    // 只有在路由表中配置了的路径（含children子路由）才能跳转，否则不进行导航
+    if (isConfigedRoute(to.path)) {
+        next()
+    }
+})
 
-const app = new Vue({ // eslint-disable-line
+const app = new Vue({
     router
 }).$mount('#app')
 
@@ -24,9 +47,9 @@ window.addEventListener('message', function (e) {
         return
     }
     if (e.data.redirectName) {
-        router.push({path: `/${e.data.redirectName}`})
+        router.push({ path: `/${e.data.redirectName}` })
     }
     if (e.data.type && e.data.type === 'GETCURRENTURL') {
-        window.parent.postMessage({demoUrl: location.href}, '*')
+        window.parent.postMessage({ demoUrl: location.href }, '*')
     }
 }, false)
